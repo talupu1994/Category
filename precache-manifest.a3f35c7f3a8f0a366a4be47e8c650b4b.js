eee9b5616237852849154d7fa20cf9d1b9f5079a
tal <lupu.tal@gmail.com>
2020-01-19 20:47:41 +0200

    Updates

---
+++ precache-manifest.a3f35c7f3a8f0a366a4be47e8c650b4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4763899224dd8d9701d9bf7044b1ce",
+    "url": "/Category/index.html"
+  },
+  {
+    "revision": "14f0a52a224cff74ee83",
+    "url": "/Category/static/css/main.7263a235.chunk.css"
+  },
+  {
+    "revision": "d306e7b36c059ef3d40e",
+    "url": "/Category/static/js/2.bf3ea05f.chunk.js"
+  },
+  {
+    "revision": "14f0a52a224cff74ee83",
+    "url": "/Category/static/js/main.e629deea.chunk.js"
+  },
+  {
+    "revision": "650a23707a7b84a6fc3f",
+    "url": "/Category/static/js/runtime-main.7515fdf7.js"
+  }
+]);
